2ba6da5dca75d7c5446eb049b311924cce90588a
Haneen-S <62634663+Haneen-S@users.noreply.github.com>
2020-07-24 21:55:41 -0500

    database tables

---
+++ database.sql
@@ -0,0 +1,60 @@
+CREATE TABLE Users(
+UserID varchar(50) NOT NULL,
+UserName varchar(255),
+PRIMARY KEY (UserID)
+);
+
+CREATE TABLE Pantry(
+Name varchar(255) NOT NULL,
+Quantity INT,
+ExpirationDate DATE,
+PRIMARY KEY(Name)
+);
+
+CREATE TABLE Recipe(
+ID INT,
+Name varchar(255),
+Instructions varchar (1000),
+PrepTime INT,
+CookTime INT,
+Servings INT,
+PRIMARY KEY(ID)
+);
+
+CREATE TABLE Ingredients(
+RecipeID INT NOT NULL,
+Name varchar(255) NOT NULL,
+Quantity FLOAT,
+Type varchar(50),
+PRIMARY KEY(Name, RecipeName),
+FOREIGN KEY(RecipeID) REFERENCES Recipe ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE MealType(
+ID INT NOT NULL,
+Type varchar(255) NOT NULL,
+PRIMARY KEY(ID, Type),
+FOREIGN KEY(ID) REFERENCES Recipe ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE DietaryRestrictions(
+ID INT NOT NULL,
+Restriction varchar(255) NOT NULL,
+PRIMARY KEY(ID, Restriction),
+FOREIGN KEY(ID) REFERENCES Recipe ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE MyRecipe(
+UserID varchar(50) NOT NULL,
+RecipeID INT,
+Name varchar(255),
+PrepTime INT,
+Instructions varchar (1000),
+MealType varchar(50),
+PRIMARY KEY(ID)
+
+FOREIGN KEY (RecipeID) REFERENCES Recipe, ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY (UserID) REFERENCES Users, ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- load excel data into Recipe, Ingredients, MealType, and DietaryRestrictions tables
