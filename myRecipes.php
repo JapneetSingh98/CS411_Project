1b73e6b565be46303e402c7904714250f2341501
Haneen-S <62634663+Haneen-S@users.noreply.github.com>
2020-08-01 16:46:23 -0500

    updated recommended recipes procedure

--- myRecipes.php
+++ myRecipes.php
@@ -10,16 +10,35 @@
     $res = mysqli_query($conn, $sql);
     while($row = mysqli_fetch_array($res)) $userID = $row['ID'];
     
-    $sql = "SELECT RecipeID, Name FROM MyRecipe WHERE UserID = '$userID'";
-    $res = mysqli_query($conn, $sql);
-
+    //reccomended recipe table (only top 3 recipes)
+    $recommendSQL = "CALL `RecommendRecipes`(\"$userID\");";
+    $recommendedRecipes = mysqli_query($conn, $recommendSQL);
     echo "<table border='1'>
     <tr>
     <th>ID</th>
     <th>Name</th>
     </tr>";
     echo "<form name=\"RecipeDetails\" action=\"recipeDetails.php\" method=\"post\">";
-    while($row = mysqli_fetch_array($res))
+    while($row = mysqli_fetch_array($recommendedRecipes))
+    {
+        $id = $row['RecipeID'];
+        echo "<tr>";
+        echo "<td><input type='Submit' name='Details' value='$id'></td>";
+        echo "<td>" . $row['RecipeName'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    
+    //My Recipe table
+    $recipeSQL = "SELECT * FROM `MyRecipe` WHERE UserID = \"$userID\"";
+    $recipes = mysqli_query($conn, $recipeSQL);
+    echo "<table border='1'>
+    <tr>
+    <th>ID</th>
+    <th>Name</th>
+    </tr>";
+    echo "<form name=\"RecipeDetails\" action=\"recipeDetails.php\" method=\"post\">";
+    while($row = mysqli_fetch_array($recipes))
     {
         $id = $row['RecipeID'];
         echo "<tr>";
